d2283d93c5a6dad3de54c732ef434cb668b0cac1
Jared McFarland <jared.online@gmail.com>
2010-11-08 22:28:58 -0700

    formatting

--- lib/inline_error_messages.rb
+++ lib/inline_error_messages.rb
@@ -8,12 +8,12 @@ module ActionView
       def error_wrapping(html_tag, has_error)
         if has_error
           if html_tag =~ /^<label/ || html_tag =~ /type="hidden"/
-                "<span class=\"errorLabel\">#{html_tag}</span>"
+            "<span class=\"errorLabel\">#{html_tag}</span>"
           else
             if has_error.respond_to?("join")
               has_error = has_error.join('<br />')
             end
-           "<div class=\"formError\">#{html_tag} <span class=\"errorMessages\">#{has_error}</span></div>"
+            "<div class=\"formError\">#{html_tag} <span class=\"errorMessages\">#{has_error}</span></div>"
           end
         else
           html_tag
